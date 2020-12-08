408276a27da52268b0f2c5cf08a241180a117242
pai-hj <dkhj513@publicai.co.kr>
2020-12-08 17:22:09 +0900

    add annotations

--- utils/datagenerator.py
+++ utils/datagenerator.py
@@ -1,8 +1,8 @@
 import numpy as np
 from tensorflow.keras.utils import Sequence
 from utils.helper import glob_all_files, random_patch, show_images, paths2numpy, images_cropper
-from utils.augmentator import face_resize_augmentation
-from utils.analysis import image_info
+from utils.helper import face_resize_augmentation
+from utils.helper import image_info
 
 
 class TightFaceProvider(Sequence):
@@ -20,21 +20,28 @@ class TightFaceProvider(Sequence):
         :param fg_folder: Tight 하게 Crop 된 월리 이미지들이 들어 있는 폴더
         :param bg_folder: 월리가 있는 부분은 block 처리된 full image 가 들어 있는 폴더
         """
+        # 지정된 배치 사이즈
         self.batch_size = batch_size
 
-        # foreground 이미지를 resize 합니다.
+        # 월리 얼굴을 경로로 가져온다.
         self.fg_imgs = glob_all_files(fg_folder)
+        # 가져온 경로에서 이미지 파일을 numpy 배열로 바꾼다.
         self.fg_imgs = paths2numpy(self.fg_imgs)
+        # numpy 배열의 월리 얼굴들을 사이즈별로 새로 생성한다.
         self.fg_imgs = face_resize_augmentation(self.fg_imgs)
 
-        # filter size, stride를 구합니다.
+        # 월리 얼굴의 최대 높이, 최대 너비를 구한다.
         (max_h, min_h), (max_w, _), (_, _) = image_info(self.fg_imgs)
+        # 높이와 너비 중 더 큰 것을 max_length에 저장한다.
         max_length = np.maximum(max_h, max_w)
+        # stride size를 설정한다.
         stride = int(max_length/4)
 
-        # background 이미지를 가져와서 일정 간격으로 crop합니다.
+        # background 이미지의 경로를 가져온다.
         self.bg_imgs = glob_all_files(bg_folder)
+        # 가져온 경로에서 이미지 파일을 numpy로 바꾼다.
         self.bg_imgs = paths2numpy(self.bg_imgs)
+        # 크기가 max_length인 filter를 siride 단위로 background 이미지를 분할한다.
         self.bg_imgs, _ = images_cropper(self.bg_imgs, stride, stride, max_length+1, max_length+1)
 
         # background 이미지를 무작위로 섞습니다.
@@ -43,28 +50,43 @@ class TightFaceProvider(Sequence):
 
     def __len__(self):
         """
-        한 epoch 당 스탭 수를 구해주는 매직 메소드입니다.
+        한 epoch 당 step 수를 구해주기 위한 매직 메소드.
+        한 epoch 당 step 수: 전체 데이터 수  / 배치 크기
         """
+        # 배치 사이즈가 절반인 이유는 step 수를 맞추기 위함
         return np.int(np.ceil(len(self.bg_imgs)/ (self.batch_size/2)))
 
 
     def __getitem__(self, index):
-        start = index * np.int(self.batch_size/2)
-        end = (index+1) * np.int(self.batch_size/2)
+        """
+        인스턴스를 인덱싱 했을 때, 인덱스에 해당하는 이미지들과 라벨들을 리턴하는 매직 메소드.
+        """
+        # 분할된 background 이미지의 인덱싱 범위를 지정한다.
+        start = index * int(self.batch_size/2)
+        end = (index+1) * int(self.batch_size/2)
 
+        # background 인덱싱 후 길이에 맞게 라벨을 붙인다.
+        # background label: 0
         bg_imgs = self.bg_imgs[start: end]
         bg_labs = np.zeros(len(bg_imgs))
 
-        n_fg_batch = self.batch_size - len(bg_imgs)
-        indices = np.random.choice(np.arange(len(self.fg_imgs)), size=n_fg_batch)
+        # foreground 이미지에 해당하는 배치 사이즈를 지정한다.
+        fg_batch = self.batch_size - len(bg_imgs)
+        # 랜덤하게 월리 얼굴을 가져오기 위한 fg_batch 개수 만큼의 인덱스를 무작위로 가져온다.
+        indices = np.random.choice(np.arange(len(self.fg_imgs)), size=fg_batch)
+        # 위에서 지정한 랜덤 인덱스에 맞는 월리 얼굴을 가져온다.
         fg_imgs = [self.fg_imgs[ind] for ind in indices]
+        # 길이에 맞게 라벨을 붙인다.
+        # foreground label: 1
         fg_labs = np.ones(len(fg_imgs))
 
+        # 기존의 월리 얼굴을 background의 랜덤한 위치에 붙여준 후, 그 이미지를 foreground 이미지로 다시 저장한다.
         for ind, img in enumerate(bg_imgs.copy()):
             fg_imgs[ind] = random_patch(img, fg_imgs[ind])
 
-        # batch_size만큼 fg, bg 이미지를 가져옵니다.
+        # batch_xs에는 background, foreground의 이미지들이 들어간다.
         batch_xs = np.concatenate([fg_imgs, bg_imgs], axis=0)
+        # batch_ys에는 background, foreground의 라벨들이 들어간다.
         batch_ys = np.concatenate([fg_labs, bg_labs], axis=0)
         return batch_xs, batch_ys
 
