d0f1bf2a971ef0dcf8e4e69493aa9629002f8f96
pai-sj <rocketgrowthsj@publicai.co.kr>
2021-02-04 17:21:52 +0900

    imgaug 적용중

--- utils/datagenerator.py
+++ utils/datagenerator.py
@@ -3,6 +3,7 @@ from tensorflow.keras.utils import Sequence
 from utils.helper import glob_all_files, paths2numpy, face_resize_augmentation, images_cropper
 from utils.helper import random_patch
 from utils.helper import show_images
+from utils.helper import image_info
 
 # from utils.helper import image_info   # 현재 사용하지 않는 외부 함수이기 때문에 주석처리 합니다.
 
@@ -35,7 +36,7 @@ class TightFaceProvider(Sequence):
         # numpy 배열의 월리 얼굴들을 사이즈별로 새로 생성한다.
         self.fg_imgs = face_resize_augmentation(self.fg_imgs)
 
-        """
+        
         # **test: 현재 crop 사이즈를 외부에서 입력받은 값을 기준으로 하기 때문에 주석처리 합니다.
         
         # 월리 얼굴의 최대 높이, 최대 너비를 구한다.
@@ -44,7 +45,7 @@ class TightFaceProvider(Sequence):
         max_length = np.maximum(max_h, max_w)
         # stride size를 설정한다.
         stride = int(max_length/4)
-        """
+        
 
         # background 이미지의 경로를 가져온다.
         self.bg_imgs = glob_all_files(bg_folder)
@@ -53,7 +54,7 @@ class TightFaceProvider(Sequence):
 
         # 가져온 경로에서 이미지 파일을 numpy로 바꾼다.
         self.bg_imgs = paths2numpy(self.bg_imgs)
-        # 크기가 max_length인 filter를 siride 단위로 background 이미지를 분할한다.
+        # 크기가 max_length인 filter를 stride 단위로 background 이미지를 분할한다.
         self.bg_imgs, _ = images_cropper(self.bg_imgs, 10, 10, 36, 36)
 
         # images_cropper로 인해 5차원 배열이 된 self.bg_imgs를 4차원 배열로 만들어주기 위한 부분
